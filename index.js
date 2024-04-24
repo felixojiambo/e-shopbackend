e0f660582e629b7037065910e6336844336e6b8a
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-24 06:11:58 -0700

    product schema

--- index.js
+++ index.js
@@ -1,45 +1,59 @@
-const port=4000;
-const express=require("express")
-const app=express()
-const mongoose=require("mongoose")
-const jwt=require("jsonwebtoken")
-const multer=require("multer")
-const path=require("path")
-const cors=require("cors")
-app.use(express.json())
-app.use(cors())
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
 
 //database connection with mongodb
-mongoose.connect("mongodb://localhost:27017/ecom")
+mongoose.connect("mongodb://localhost:27017/ecom");
 
-app.get("/",(request,response)=>{
-    res.send("Express App is running on port 4000")
+app.get("/", (request, response) => {
+  res.send("Express App is running on port 4000");
+});
 
-})
+//schema for creating producst
+const Product = mongoose.model("Product", {
+  id: { type: Number, required: true },
+  name: { type: String, required: true },
+  image: { type: String, reuired: true },
+  category: { type: String, required: true },
+  new_prices: { type: Number, required: true },
+  old_price: { type: Number, required: true },
+  date: { type: Date, default: Date.now() },
+  available: { type: boolean, default: true },
+});
 
-const upload=multer({storage:storage})
+const upload = multer({ storage: storage });
 //creating upload images
-app.use('/images',express.static('upload/images'))
-app.post("/upload",upload.single('product'),(req,res)=>{
-    res.json({
-        success:1,
-image_url:`http://localhost:${port}/images/${req.file.filename}`
-    })
-})
-
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+//524
 //api creation
-app.listen(port,(error)=>{
-    if(!error){
-        console.log("Server running on port: "  +port)
-    }else{
-        console.log("Error: "+error)
-    }
-})
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server running on port: " + port);
+  } else {
+    console.log("Error: " + error);
+  }
+});
 
 //image storage
-const storage=multer.diskStorage({
-    destination:'./upload/images',
-    filename:(req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
