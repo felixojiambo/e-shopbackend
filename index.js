364d84a4d9a468e8d0ce8761453d8aabfdd2154d
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-27 22:20:24 -0700

    remove product

--- index.js
+++ index.js
@@ -29,17 +29,17 @@ const Product = mongoose.model("Product", {
 });
 //Add product
 app.post("/addproduct", async (req, res) => {
-  let products= await Product.find({});
+  let products = await Product.find({});
   let id;
-  if(products.length){
-    let last_product_array=products.slice(-1);
-    let last_product=last_product_array[0];
-    id=last_product.id+1;
-  }else{
-    id=1;
+  if (products.length) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
   }
   const product = new Product({
-    id:id,
+    id: id,
     name: req.body.name,
     category: req.body.category,
     new_price: req.body.new_price,
@@ -49,6 +49,13 @@ app.post("/addproduct", async (req, res) => {
   await product.save();
   console.log("Saved");
   res.json({ success: true, name: req.body.name });
+});
+
+//deleting product
+app.post("/removeproducts", async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  console.log("removed");
+  res.jsons({ success: true, name: req.body.name });
 });
 const upload = multer({ storage: storage });
 //creating upload images
