ceda69955804793cd253429de05c5c09fc6c4d8a
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-28 01:53:34 -0700

    add product by admin page

--- admin/src/pages/admin/Admin.jsx
+++ admin/src/pages/admin/Admin.jsx
@@ -2,10 +2,17 @@
 import React from 'react'
 import './Admin.css'
 import Sidebar from '../../components/sidebar/Sidebar'
+import AddProduct from '../../components/addproduct/AddProduct'
+import {Routes,Route} from  'react-router-dom'
+import ListProduct from '../../components/listproduct/ListProduct'
 const Admin = () => {
   return (
     <div className="admin">
     <Sidebar />
+    <Routes>
+      <Route path='/addproduct' element={<AddProduct/>} />
+      <Route path='/LISTproduct' element={<ListProduct/>} />
+    </Routes>
     </div>
   )
 }
