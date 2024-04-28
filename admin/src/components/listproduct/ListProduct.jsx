5cf27d82dc49990c21c280f5f7b4346284e44cdd
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-28 04:08:13 -0700

    list products format

--- admin/src/components/listproduct/ListProduct.jsx
+++ admin/src/components/listproduct/ListProduct.jsx
@@ -1,12 +1,25 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import './ListProduct.css'
+import React from "react";
+import "./ListProduct.css";
 const ListProduct = () => {
+  
   return (
-    <div className='list-product'>
-        
-    </div>
-  )
-}
+    <div className="list-product">
+      <h1>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
 
-export default ListProduct
+      </div>
+    </div>
+  );
+};
+
+export default ListProduct;
