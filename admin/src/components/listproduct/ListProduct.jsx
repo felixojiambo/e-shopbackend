1410dc77a47445ca167e31c7250a36b4ec2584bd
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-28 05:08:19 -0700

    list fetch api

--- admin/src/components/listproduct/ListProduct.jsx
+++ admin/src/components/listproduct/ListProduct.jsx
@@ -1,8 +1,29 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ListProduct.css";
+import cross_icon from '../../assets/cross_icon.
+<ul class="pagination pagination-lg">
+  <li><a href="#">&laquo;</a></li>
+  <li><a href="#">1</a></li>
+  <li><a href="#">2</a></li>
+  <li><a href="#">3</a></li>
+  <li><a href="#">4</a></li>
+  <li><a href="#">5</a></li>
+  <li><a href="#">&raquo;</a></li>
+</ul>
+'
 const ListProduct = () => {
-  
+  const [allproducts, setAllProducts] = useState([]);
+  const fetchInfo = async () => {
+    await fetch("http://localhost:4000/allproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data);
+      });
+  };
+  useEffect(() => {
+    fetchInfo();
+  }, []);
   return (
     <div className="list-product">
       <h1>All Products List</h1>
@@ -16,7 +37,25 @@ const ListProduct = () => {
       </div>
       <div className="listproduct-allproducts">
         <hr />
-
+        {allproducts.map((product, index) => {
+          return (
+            <div
+              key={index}
+              className="listproduct-format-main listproduct-format"
+            >
+              <img
+                src={product.image}
+                alt=""
+                className="listproduct-product-icon"
+              />
+              <p>{product.name}</p>
+              <p>KES{product.old_price}</p>
+              <p>KES{product.new_price}</p>
+              <p>{product.category}</p>
+              <img className='listproduct-remove-icon' src={cross_icon} alt=" "/>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
