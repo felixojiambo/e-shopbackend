8918abad7e49f82dfb7d6d20ba829fa550eedf2b
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-28 06:34:39 -0700

    register user,generate token and add salt

--- index.js
+++ index.js
@@ -99,7 +99,30 @@ const users=mongoose.model('users',{
   },date:{type:Date,default:Date.now,}
 }
 )
-
+//register user endpoint
+app.post('/signup',async(req,res)=>{
+  let check=await Users.findOne({email:req.body.email});
+  if (check){
+    return res.status(400).json({success:false,errors:"existing user found with same email address"})
+  }let cart ={};
+  for(let i=0;i<300;i++){
+    cart[i]=0;
+  }
+  const user =new Users({
+    name:req.body.username,
+    email:req.body.email,
+    password:req.body.password,
+    cartData:cart,
+  })
+  await user.save();
+  const data={
+    user:{
+      id:user.id
+    }
+  }
+  const token=jwt.sign(data,'secret_ecom');
+  res.json({success:true,token});
+})
 
 //creating upload images
 app.use("/images", express.static("upload/images"));
