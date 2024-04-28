7c4eb756dd2db74211ed12ceb60cb08d602789e0
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-28 00:27:44 -0700

    add sidebar and mount it +mount admin page in app

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Admin from "./pages/admin/Admin";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Admin />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- admin/src/pages/admin/Admin.jsx
+++ admin/src/pages/admin/Admin.jsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './Admin.css'
+import Sidebar from '../../components/sidebar/Sidebar'
 const Admin = () => {
   return (
     <div className="admin">
-    
+    <Sidebar />
     </div>
   )
 }
