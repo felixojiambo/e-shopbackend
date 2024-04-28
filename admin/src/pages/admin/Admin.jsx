b08bd2a7b25bb025eabd35eb078bdaa9c26b4c16
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-28 00:04:32 -0700

    admin page setup

---
+++ admin/src/pages/admin/Admin.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Admin.css'
+const Admin = () => {
+  return (
+    <div className="admin">
+    
+    </div>
+  )
+}
+
+export default Admin
